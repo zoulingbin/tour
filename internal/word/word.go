45c425a504abfd8d48050c892a071037ec3594bb
zoulingbin <40159060+zoulingbin@users.noreply.github.com>
2021-03-26 19:28:27 +0800

    字符串转换

--- internal/word/word.go
+++ internal/word/word.go
@@ -1,6 +1,9 @@
 package word
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 //单词全部转为小写/大写
 func ToUpper(s string) string {
@@ -11,4 +14,32 @@ func ToLower(s string) string {
 	return strings.ToLower(s)
 }
 
+//下划线转大写驼峰
+func UnderscoreToUpperCamelCase(s string) string {
+	s = strings.Replace(s,"_", " ",-1)
+	s = strings.Title(s)
+	return strings.Replace(s," ", "", -1)
+}
+
+//下划线转小写驼峰
+func UnderscoreToLowerCamelCase(s string) string {
+	s = UnderscoreToUpperCamelCase(s)
+	return string(unicode.ToLower(rune(s[0]))) + s[1:]
+}
+
+//驼峰转下划线
+func CamelCaseToUnderscore(s string) string {
+	var output []rune
+	for i, r := range s {
+		if i == 0 {
+			output = append(output, unicode.ToLower(r))
+			continue
+		}
+		if unicode.IsUpper(r) {
+			output = append(output, '_')
+		}
+		output = append(output, unicode.ToLower(r))
+	}
+	return string(output)
+}
 
