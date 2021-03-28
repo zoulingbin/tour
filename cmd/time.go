bf26d14a06c03a6b012ef17083f868903c115f5f
zoulingbin <40159060+zoulingbin@users.noreply.github.com>
2021-03-29 00:59:29 +0800

    time子命令

--- cmd/time.go
+++ cmd/time.go
@@ -1,21 +1,72 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"github.com/zoulingbin/tour/internal/timer"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
 
 var timeCmd = &cobra.Command{
 	Use: "time",
 	Short: "时间格式处理",
 	Long: "时间格式处理",
-	Run:timeRun,
+	Run: func(cmd *cobra.Command, args []string) {},
 }
 
+var nowTimeCmd = &cobra.Command{
+	Use: "now",
+	Short: "获取当前时间",
+	Long: "获取当前时间",
+	Run: func(cmd *cobra.Command, args []string) {
+		nowTime := timer.GetNowTime()
+		log.Printf("输出结果：%s,%d",nowTime.Format("2006-01-02 15:01:01"),nowTime.Unix())
+	},
+}
 
+var calculateTimeCmd = &cobra.Command{
+	Use: "clac",
+	Short: "计算所需时间",
+	Long: "计算所需时间",
+	Run: func(cmd *cobra.Command, args []string) {
+			currentTimer := time.Time{}
+			layout := "2006-01-02 15:04:05"
+			if calculateTime == "" {
+				currentTimer = timer.GetNowTime()
+			}else{
+				var err error
+				space := strings.Count(calculateTime, " ")
+				if space == 0 {
+					layout = "2006-01-02"
+				}
+				if space == 1 {
+					layout = "2006-01-02 15:04:05"
+				}
+				currentTimer, err = time.Parse(layout, calculateTime)
+				if err != nil {
+					t, _ := strconv.Atoi(calculateTime)
+					currentTimer = time.Unix(int64(t), 0)
+				}
+			}
+			t, err := timer.GetCalculateTime(currentTimer, duration)
+			if err != nil {
+				log.Fatal("timer.GetCalculateTime err: %v", err)
+			}
+			log.Printf("输出结果: %s, %d", t.Format(layout), t.Unix())
+	},
+
+}
 
 var calculateTime string
 var duration string
 
 
+func init() {
+	timeCmd.AddCommand(nowTimeCmd)
+	timeCmd.AddCommand(calculateTimeCmd)
 
-func timeRun(cmd *cobra.Command, args []string) {
-
+	calculateTimeCmd.Flags().StringVarP(&calculateTime, "calculate", "c", "", `需要计算的时间，有效单位为时间戳或已格式化后的时间`)
+	calculateTimeCmd.Flags().StringVarP(&duration, "duration", "d", "", `持续时间，有效时间单位为"ns", "us" (or "µs"), "ms", "s", "m", "h"`)
 }
